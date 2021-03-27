3059acd10edb32c9ef834ae38ca4d12b7372c16f
Timmy <70575836+timmypelumy@users.noreply.github.com>
2021-03-28 00:33:00 +0100

    adult step

--- lion/urls.py
+++ lion/urls.py
@@ -5,9 +5,11 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from config import admin_site1
 
 urlpatterns = [
     path('bank-admin/', admin_site1.urls ),
     path( "", include('banking.urls') )
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
