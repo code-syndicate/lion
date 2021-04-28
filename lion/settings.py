ec35e194eba36798151b46efc1fc16c0f500f6e0
Timmy <70575836+timmypelumy@users.noreply.github.com>
2021-04-28 23:13:17 +0100

    changes

--- lion/settings.py
+++ lion/settings.py
@@ -142,7 +142,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '465'
+EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'truecitizenbank@gmail.com'
 EMAIL_HOST_PASSWORD = 'truecitizenbanksupport'
 EMAIL_USE_TLS = True
